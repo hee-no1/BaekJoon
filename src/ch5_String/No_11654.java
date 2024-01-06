6546eabc736f7fddf902a351d431599c8fbb3fc1
hee-no1 <lhw817@naver.com>
2024-01-06 16:54:01 +0900

    [refactor]No_11654 아스키 코드

--- src/ch5_String/No_11654.java
+++ src/ch5_String/No_11654.java
@@ -17,7 +17,7 @@ import java.io.InputStreamReader;
 public class No_11654 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char c = br.readLine().charAt(0);
-        System.out.println(c -'A'+65);
+        int c = br.readLine().charAt(0);
+        System.out.println(c);
     }
 }
