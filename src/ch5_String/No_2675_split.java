124220b94dcd3fb7375b472a76e6f2988f3928bc
hee-no1 <lhw817@naver.com>
2024-01-07 18:29:31 +0900

    No_2675 문자열 반복 (split 사용)

--- src/ch5_String/No_2675_split.java
+++ src/ch5_String/No_2675_split.java
@@ -3,7 +3,6 @@ package ch5_String;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 /** No_2675 문자열 반복
  * 문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
@@ -15,15 +14,14 @@ import java.util.StringTokenizer;
  * 출력
  * 각 테스트 케이스에 대해 P를 출력한다.
  */
-
-public class No_2675 {
+public class No_2675_split {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for(int i=0;i<T;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine()," ");
-            int R = Integer.parseInt(st.nextToken());
-            String str = st.nextToken();
+            String[] arr = br.readLine().split(" ");
+            int R = Integer.parseInt(arr[0]);
+            String str = arr[1];
 
             //str의 글자들을 R번씩 반복해야한다.
             //1.str의 글자 한 자리씩 접근해야한다.
