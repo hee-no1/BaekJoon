e113e41d5d9325cd1b1e30d1e8c0b36e1c41a5ff
hee-no1 <lhw817@naver.com>
2024-01-15 13:05:41 +0900

    No_24262

---
+++ src/ch11_timeComplexity/No_24262.java
@@ -0,0 +1,10 @@
+package ch11_timeComplexity;
+
+/** No_24262
+ */
+public class No_24262 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
