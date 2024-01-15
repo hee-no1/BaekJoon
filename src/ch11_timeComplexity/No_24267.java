0b05a9631c85fcf1a16b46efb3f68f0bea4dea8c
hee-no1 <lhw817@naver.com>
2024-01-15 14:46:59 +0900

    No_24267

---
+++ src/ch11_timeComplexity/No_24267.java
@@ -0,0 +1,17 @@
+package ch11_timeComplexity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/** No_24267
+ * 시간 복잡도 : O(N^3)
+ */
+public class No_24267 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        System.out.println((n*(n-1)*(n-2))/6);  //nC3과 같다. 1~n까지 중복 없이 숫자를 3개를 뽑고, 그것을 크기 순으로 나열하는 것과 같다.
+        System.out.println(3);
+    }
+}
