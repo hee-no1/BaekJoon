31462972e3d64e86ae6e4a09c17d52d4193588d1
hee-no1 <lhw817@naver.com>
2024-01-15 13:29:55 +0900

    No_24265

--- src/ch11_timeComplexity/No_24265.java
+++ src/ch11_timeComplexity/No_24265.java
@@ -4,14 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/** No_24264
- * 시간 복잡도: O(N^2) - for문 2개
- * 주의: int타입으로 계산할 때 결과값이 거질거같으면 long으로 타입 변환을 해야한다.
+/** No_24265
+ * 1~n까지의 자연수의 합 = n(n+1)/2
  */
-public class No_24264 {
+public class No_24265 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println((long)Math.pow(Double.parseDouble(br.readLine()), 2));
+        long n = Long.parseLong(br.readLine());
+        System.out.println(n*(n+1)/2 -n);
         System.out.println(2);
     }
 }
---
+++ src/ch12_bruteForce/No_2798.java
@@ -0,0 +1,42 @@
+package ch12_bruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/** No_2798 블랙잭
+ * 이제 플레이어는 제한된 시간 안에 N장의 카드 중에서 3장의 카드를 골라야 한다. 블랙잭 변형 게임이기 때문에, 플레이어가 고른 카드의 합은 M을 넘지 않으면서 M과 최대한 가깝게 만들어야 한다.
+ * N장의 카드에 써져 있는 숫자가 주어졌을 때, M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 구해 출력하시오.
+
+ * 입력
+ * 첫째 줄에 카드의 개수 N(3 ≤ N ≤ 100)과 M(10 ≤ M ≤ 300,000)이 주어진다. 둘째 줄에는 카드에 쓰여 있는 수가 주어지며, 이 값은 100,000을 넘지 않는 양의 정수이다.
+ * 합이 M을 넘지 않는 카드 3장을 찾을 수 있는 경우만 입력으로 주어진다.
+
+ * 출력
+ * 첫째 줄에 M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 출력한다.
+ */
+public class No_2798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int count = 0;
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0;i<arr.length;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        while(count++<3){
+            for(int i=0;i<arr.length;i++){
+                if(M-2>arr[i]){
+
+                }
+            }
+        }
+    }
+}
