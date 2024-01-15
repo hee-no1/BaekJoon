8640320e9f157bd532f7bdb022b98f014233ea58
hee-no1 <lhw817@naver.com>
2024-01-15 13:37:37 +0900

    No_24266

--- src/ch11_timeComplexity/No_24266.java
+++ src/ch11_timeComplexity/No_24266.java
@@ -1,18 +1,18 @@
 package ch11_timeComplexity;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/** No_24265
- * 1~n까지의 자연수의 합 = n(n+1)/2
- * 시간복잡도: O(N^2)
+/** No_24266
+ * 시간 복잡도: O(N^3) - for문 3번
  */
-public class No_24265 {
+public class No_24266 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long n = Long.parseLong(br.readLine());
-        System.out.println(n*(n-1)/2);  //n*(n+1)/2 -n
-        System.out.println(2);
+        System.out.println(n*n*n);
+        System.out.println(3);
     }
 }
