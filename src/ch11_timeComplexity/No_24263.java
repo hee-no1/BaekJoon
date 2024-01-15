9421e21b4b6a3e54b47cc6a8c03278fb1853e1de
hee-no1 <lhw817@naver.com>
2024-01-15 13:09:42 +0900

    No_24263

---
+++ src/ch11_timeComplexity/No_24263.java
@@ -0,0 +1,15 @@
+package ch11_timeComplexity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/** No_24263
+ */
+public class No_24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()));
+        System.out.println(1);
+    }
+}
