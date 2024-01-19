0624850cf60dd63a3b4bef44787a50dc882e8527
hee-no1 <lhw817@naver.com>
2024-01-19 09:39:32 +0900

    No_2587 대표값2

---
+++ src/ch13_sort/No_2587.java
@@ -0,0 +1,32 @@
+package ch13_sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/** No_2587 대표값2
+ * 다섯 개의 자연수가 주어질 때 이들의 평균과 중앙값을 구하는 프로그램을 작성하시오.
+
+ * 입력
+ * 첫째 줄부터 다섯 번째 줄까지 한 줄에 하나씩 자연수가 주어진다. 주어지는 자연수는 100 보다 작은 10의 배수이다.
+
+ * 출력
+ * 첫째 줄에는 평균을 출력하고, 둘째 줄에는 중앙값을 출력한다. 평균과 중앙값은 모두 자연수이다
+ */
+public class No_2587 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[5];
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        br.close();
+        Arrays.sort(arr);
+
+        System.out.println(sum/5);
+        System.out.println(arr[2]);
+    }
+}
