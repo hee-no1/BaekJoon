39ceaa64e386618efd136d04d924d6d0bdd127af
hee-no1 <lhw817@naver.com>
2024-01-20 19:00:58 +0900

    No_2751 수 정렬하기 2 (Collection.sort()이용)

--- src/ch13_sort/No_2751_v2_Collections_sort.java
+++ src/ch13_sort/No_2751_v2_Collections_sort.java
@@ -1,7 +1,9 @@
 package ch13_sort;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 /** No_2751 수 정렬하기 2
  * N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
@@ -12,22 +14,22 @@ import java.util.Arrays;
  * 출력
  * 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
  *
- * Arrays.sort()를 이용하면
+ * Collection.sort()를 이용하면
  * println은 시간초과가 나는데 BufferedWriter을 이용하면 시간초과가 안난다.
  */
-public class No_2751 {
+public class No_2751_v2_Collections_sort {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        ArrayList<Integer> list = new ArrayList<>();
 
         for(int i=0;i<N;i++){
-            arr[i] = Integer.parseInt(br.readLine());
+            list.add(Integer.parseInt(br.readLine()));
         }
 
-        Arrays.sort(arr);
-        for(int i:arr){
+        Collections.sort(list);
+        for(int i:list){
             bw.write(i + "\n");
         }
         bw.flush();
