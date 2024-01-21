d3d162dff8676c22316ea7feb2cbdc3a68e88d4b
hee-no1 <lhw817@naver.com>
2024-01-21 20:59:36 +0900

    No_1427 소트인사이드 ver2

--- src/ch13_sort/No_1427_v2.java
+++ src/ch13_sort/No_1427_v2.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 
 /** No_1427 소트인사이드
  * 배열을 정렬하는 것은 쉽다. 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정렬해보자.
@@ -15,22 +14,17 @@ import java.util.Collections;
  * 출력
  * 첫째 줄에 자리수를 내림차순으로 정렬한 수를 출력한다.
 
- * 시간 128ms
+ * 시간 120ms
  */
-public class No_1427 {
+public class No_1427_v2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        Character[] arr = new Character[str.length()];
+        char[] arr = br.readLine().toCharArray();  //String -> char[]
 
-        for(int i=0;i<arr.length;i++){
-            arr[i] = str.charAt(i);
-        }
-        Arrays.sort(arr,Collections.reverseOrder());
-        //Collections.reverseOrder() 사용하려면 arr의 래퍼클래스 배열이어야한다.
+        Arrays.sort(arr);
 
-        for(char c : arr){
-            System.out.print(c-'0');
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]);
         }
     }
 }
