ab5897c2df93490abc129b6d18b013255967a226
hee-no1 <lhw817@naver.com>
2024-01-24 10:49:15 +0900

    No_10814 나이 순 정렬 (객체를 만들어서 객체 배열 이용, BufferedWriter 이용)

--- src/ch13_sort/No_10814_v3_Instance_BufferedWriter.java
+++ src/ch13_sort/No_10814_v3_Instance_BufferedWriter.java
@@ -3,6 +3,7 @@ package ch13_sort;
 import java.io.*;
 import java.util.StringTokenizer;
 import java.util.Arrays;
+
 /** No_10814 나이 순 정렬
  * 온라인 저지에 가입한 사람들의 나이와 이름이 가입한 순서대로 주어진다. 이때, 회원들을 나이가 증가하는 순으로, 나이가 같으면 먼저 가입한 사람이 앞에 오는 순서로 정렬하는 프로그램을 작성하시오.
 
@@ -14,40 +15,39 @@ import java.util.Arrays;
  * 출력
  * 첫째 줄부터 총 N개의 줄에 걸쳐 온라인 저지 회원을 나이 순, 나이가 같으면 가입한 순으로 한 줄에 한 명씩 나이와 이름을 공백으로 구분해 출력한다.
 
- * 시간: 1544ms
+ * 시간: 728ms
  */
-
-public class No_10814_v2_Instance_println {
+public class No_10814_v3_Instance_BufferedWriter {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
 
-        //Member 객체의 배열을 만들어서 거기에 입력값을 저장한다.
-        Member[] members = new Member[N]; //객체 배열을 안에 객체를 생성해준 것은 아니다.
+        Member[] members = new Member[N]; //객체 배열
         for(int i=0;i<N;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            members[i] = new Member(Integer.parseInt(st.nextToken()), st.nextToken()); //객체 생성
+            members[i] = new Member(Integer.parseInt(st.nextToken()),st.nextToken());
         }
-
-        Arrays.sort(members); //이 Comparable을 통해 Member객체를 구현할 때 이미 compareTo를 오버라이딩 했다.
+        br.close();
+        Arrays.sort(members);
 
         for(Member member : members){
-            System.out.println(member.age + " " + member.name);
+            bw.write(member.age + " " + member.name);
+            bw.newLine();
         }
+        bw.flush();
+        bw.close();
     }
 
     static class Member implements Comparable<Member>{
-        int age; //나이
-        String name; //이름
-
+        int age;
+        String name;
         Member(int age, String name){
             this.age = age;
             this.name = name;
         }
-
-        //1. 나이순  2. 나이가 같으면 입력순 -> 입력순을 자연스러운 것으로 굳이 조건식을 따로 안줘도 된다.
         @Override
-        public int compareTo(Member m) {
+        public int compareTo(Member m){
             return this.age - m.age;
         }
     }
