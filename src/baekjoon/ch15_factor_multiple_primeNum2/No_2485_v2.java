149b1b2138ac26fa6c048567a9a0381d8523b9e7
hee-no1 <lhw817@naver.com>
2024-02-01 09:08:48 +0900

    No_2485 가로수 (가로수 거리 간격을 배열에 저장 x)

--- src/baekjoon/ch15_factor_multiple_primeNum2/No_2485_v2.java
+++ src/baekjoon/ch15_factor_multiple_primeNum2/No_2485_v2.java
@@ -3,10 +3,8 @@ package baekjoon.ch15_factor_multiple_primeNum2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 
-/** No_2485 가로수
+/** No_2485 가로수 (가로수 거리 간격을 배열에 저장 x)
  * 직선으로 되어있는 도로의 한 편에 가로수가 임의의 간격으로 심어져있다. KOI 시에서는 가로수들이 모두 같은 간격이 되도록 가로수를 추가로 심는 사업을 추진하고 있다. KOI 시에서는 예산문제로 가능한 한 가장 적은 수의 나무를 심고 싶다.
  * 편의상 가로수의 위치는 기준점으로 부터 떨어져 있는 거리로 표현되며, 가로수의 위치는 모두 양의 정수이다.
  * 예를 들어, 가로수가 (1, 3, 7, 13)의 위치에 있다면 (5, 9, 11)의 위치에 가로수를 더 심으면 모든 가로수들의 간격이 같게 된다. 또한, 가로수가 (2, 6, 12, 18)에 있다면 (4, 8, 10, 14, 16)에 가로수를 더 심어야 한다.
@@ -18,32 +16,27 @@ import java.util.Comparator;
  * 출력
  * 모든 가로수가 같은 간격이 되도록 새로 심어야 하는 가로수의 최소수를 첫 번째 줄에 출력한다.
  */
-public class No_2485 {
+public class No_2485_v2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Integer[] arr = new Integer[N];
-        Integer[] distance = new Integer[N-1];
+        int[] arr = new int[N];
 
         for(int i=0;i<N;i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        //인접 인덱스끼리의 차를 구해서 그것들의 최대 공약수를 구하자
+        //가로수의 간격들의 최대 공약수를 구해야하는데 간격들을 저장하는 배열을 따로 만들지 말자.
+        int distance;
+        int gcd = 0; // 0%9 == 0, 9%0은 오류발생
         for(int i=0;i<arr.length-1;i++){
-            distance[i] = arr[i+1] - arr[i];
+            distance = arr[i + 1] - arr[i];
+            gcd = gcd(distance, gcd);
         }
 
-        //내림차순으로 정렬
-        Arrays.sort(distance,Comparator.reverseOrder()); //Comparator.reverseOrder()은 int[]에서는 사용x
-
-        int gcd = distance[0];
-        for(int i=0;i<distance.length-1;i++){
-            gcd = gcd(gcd, distance[i + 1]);
-        }
-
-        //arr[0]와 arr[arr.length-1]차를 gcd로 나눈 값 - arr.length + 1
-        int result = (arr[arr.length-1] - arr[0])/gcd - arr.length + 1;
+        //arr[0]와 arr[arr.length-1]차를 gcd로 나눈 값 + 1 - arr.length
+        //간격이 5개 나오면 필요한 나무는 6개 거기에 원래 있던 가로수의 개수를 빼준 것
+        int result = (arr[arr.length-1] - arr[0])/gcd +1  - arr.length;
         System.out.println(result);
     }
 
