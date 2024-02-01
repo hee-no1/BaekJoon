b3f8c743d17e9adcfdcc3396854d7e5d6523da7c
hee-no1 <lhw817@naver.com>
2024-02-01 12:06:35 +0900

    No_4134 다음 소수 (반복문 두번 - O(N√N))

--- src/baekjoon/ch15_factor_multiple_primeNum2/No_4134_v2_root.java
+++ src/baekjoon/ch15_factor_multiple_primeNum2/No_4134_v2_root.java
@@ -13,7 +13,7 @@ import java.io.InputStreamReader;
  * 출력
  * 각각의 테스트 케이스에 대해서 n보다 크거나 같은 소수 중 가장 작은 소수를 한 줄에 하나씩 출력한다.
  */
-public class No_4134_v1_TimeOver {
+public class No_4134_v2_root {
     public static void main(String[] args) throws IOException {
         //정수n 타입을 long으로 하자.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,25 +28,27 @@ public class No_4134_v1_TimeOver {
 
         System.out.println(sb);
     }
-
     //다음 소수를 찾는 메서드
     private static long nextPrimeNumber(long num){
         if(num <= 2){ //num이 0과 1일때 다음 소수는 2
             return 2;
+        } else if(num == 3){ //num 2, 3일때는 다음 소수 자기자신
+            return num;
         }
+        //소수 찾을 때 2부터 약수로 가지는지 확인해봐야하는데 시간을 줄이기위해 루트N까지만 확인한다.
+        //이렇게 되면 num이 2와 3일때는 루트N이 2보다 작으므로 원하는 값이 나오질 않는다. 그래서 따로 정의해준 것
 
         //이중 반복문 사용
         boolean primeNum = false;
         while(!primeNum){ //소수가 아니면 계속 반복
             int i = 2;
-            while(i < num){ //등호 붙이면 안된다. 붙이는 순간 마지막에 무조건 primeNum은 false가 나온다.
+            while(i <= Math.sqrt(num)){
                 if(num % i == 0){
                     primeNum = false;
                     break;
-                }else{
-                    i++;
-                    primeNum = true;
                 }
+                i++;
+                primeNum = true;
             }
             num++;
         }
